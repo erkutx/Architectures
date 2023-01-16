35fba054564360d1fe8b9527a93dafe5ec97308c
Erkut Cetiner <erkutcetiner0@gmail.com>
2023-01-16 23:59:52 +0300

    First commit

---
+++ MVVM/MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Erkut Cetiner on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
